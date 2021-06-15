7570b63a5d86cd2985a1ce8299591d954f1c9b1b
Samuel Dysch <samuel.dysch@cern.ch>
2021-06-15 14:29:57 +0200

    Modify setup script

--- setup.sh
+++ setup.sh
@@ -2,5 +2,10 @@
 command -v setupATLAS > /dev/null || (export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase && alias setupATLAS="source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh")
 setupATLAS
 lsetup git
-asetup AthGeneration,21.6.36
+if [ -f .asetup.save ]; then
+	asetup --restore
+else
+	asetup 21.6.67,AthGeneration
+fi
 export RIVET_ANALYSIS_PATH=$PWD
+source setupRivet.sh
