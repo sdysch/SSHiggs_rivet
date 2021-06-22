39daa5bd78843cca2a97774e7df563ede04176db
Samuel Dysch <samuel.dysch@cern.ch>
2021-06-22 18:24:51 +0200

    Add plotting script for W

---
+++ scripts/plot_schan_W.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+rivet-mkhtml -c SSHiggs.plot -o plots_schan_W outputs/schan_W_ll_eemumu_MHPPL_100GEV.yoda:"Title:100 GeV" \
+outputs/schan_W_ll_eemumu_MHPPL_200GEV.yoda:"Title:200 GeV" \
+outputs/schan_W_ll_eemumu_MHPPL_300GEV.yoda:"Title:300 GeV" \
+outputs/schan_W_ll_eemumu_MHPPL_400GEV.yoda:"Title:400 GeV" \
+#outputs/schan_W_ll_eemumu_MHPPL_500GEV.yoda:"Title:500 GeV" \
+#outputs/schan_W_ll_eemumu_MHPPL_600GEV.yoda:"Title:600 GeV" \
+#outputs/schan_W_ll_eemumu_MHPPL_700GEV.yoda:"Title:700 GeV" \
+#outputs/schan_W_ll_eemumu_MHPPL_800GEV.yoda:"Title:800 GeV"
