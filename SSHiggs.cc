9839befd20a1cc742e42961f402a0f671b2191b9
Samuel Dysch <samuel.dysch@cern.ch>
2021-06-24 14:36:07 +0200

    Silly fix

--- SSHiggs.cc
+++ SSHiggs.cc
@@ -186,7 +186,7 @@ namespace Rivet {
 
       // === fill histograms ===
 	 // lepton, electron, muon pT, eta
-      for (int i = 0; i < 3; ++i) {
+      for (int i = 0; i < 4; ++i) {
 
          // leptons
          if (nLeptons > i) {
